eea63ed54c0664eab3ba1aabb857e5cc4a730465
orieasy1 <easy1nhard2@gmail.com>
2025-06-14 03:03:41 +0900

    Fix: case-sensitive import for User model

--- backend/models/index.js
+++ backend/models/index.js
@@ -7,7 +7,7 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 // 모델 불러오기
-db.User = require('./User')(sequelize, Sequelize.DataTypes);
+db.User = require('./user')(sequelize, Sequelize.DataTypes);
 db.Goal = require('./goal')(sequelize, Sequelize.DataTypes);
 db.Tracking = require('./tracking')(sequelize, Sequelize.DataTypes);           // 추가 ✅
 db.TrackingDetails = require('./trackingDetails')(sequelize, Sequelize.DataTypes); // 추가 ✅
